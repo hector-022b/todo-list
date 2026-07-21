a4ab4293ee4927dbc4e9ea9a42f2463c191d96af
Hector Barahona <127160503+hector-022b@users.noreply.github.com>
2026-07-21 19:38:09 -0400

    Fix TodoForm label

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -2,7 +2,7 @@ function TodoForm() {
     return (
         <form>
             <label htmlFor="todoTitle">Todo</label>
-            <input type="text" id="todotitle" />
+            <input type="text" id="todoTitle" />
             <button type="submit" disabled>Add Todo</button>
         </form>
     );
