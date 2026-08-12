7e478c5fd882d77626458b750535501f6e2c9253
Hector Barahona <127160503+hector-022b@users.noreply.github.com>
2026-08-12 18:31:01 -0400

    refactor todo title validation into helper

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+
 function TodoListItem({ todo, onCompleteTodo }) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [workingTitle, setWorkingTitle] = useState(todo.title);
+
     return (
-        <li>
-            <input
-                type="checkbox"
-                checked={todo.isCompleted}
-                onChange={() => onCompleteTodo(todo.id)}
-            />
-            {todo.title}
+    <li>
+        <form>
+            {isEditing ? (
+                <TextInputWithLabel
+                    elementId={`editTodo${todo.id}`}
+                    labelText="Edit Todo"
+                    value={workingTitle}
+                    onChange={(event) => setWorkingTitle(event.target.value)}
+                />
+            ) : (
+                <>
+                    <label>
+                        <input
+                            type="checkbox"
+                            id={`checkbox${todo.id}`}
+                            checked={todo.isCompleted}
+                            onChange={() => onCompleteTodo(todo.id)}
+                        />
+                    </label>
+
+                    <span onClick={() => setIsEditing(true)}>
+                        {todo.title}
+                    </span>
+                </>
+            )}
+        </form>
         </li>
     );
 }
