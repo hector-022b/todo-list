2ca4074e8d0564265bc86b7b4e56225a0224882b
Hector Barahona <127160503+hector-022b@users.noreply.github.com>
2026-08-25 22:34:56 -0400

    Completed Lesson 08 sorting search and performance optimization

--- src/features/Todos/TodoList/TodoList.jsx
+++ src/features/Todos/TodoList/TodoList.jsx
@@ -1,12 +1,26 @@
+import { useMemo } from 'react';
 import TodoListItem from './TodoListItem.jsx';
 
-function TodoList({ todoList, onCompleteTodo, onUpdateTodo }) {
+function TodoList({
+  todoList,
+  onCompleteTodo,
+  onUpdateTodo,
+  dataVersion,
+}) {
+  const filteredTodoList = useMemo(() => {
+
+    return {
+      version: dataVersion,
+      todos: todoList.filter(todo => !todo.isCompleted),
+    };
+  }, [todoList, dataVersion]);
+
   return (
-    todoList.length === 0 ? (
+    filteredTodoList.todos.length === 0 ? (
       <p>Add todo above to get started</p>
     ) : (
       <ul>
-        {todoList.map(todo => (
+        {filteredTodoList.todos.map(todo => (
           <TodoListItem
             key={todo.id}
             todo={todo}
